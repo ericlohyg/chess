aee07bfafc4a2bc2b71182e1ed731cbe37a7a757
Eric Loh <ericlohyg@gmail.com>
2020-01-21 00:04:27 +0800

    again

--- server/main.js
+++ server/main.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
 });
-server.listen(80, function() { });
+server.listen(1337, function() { });
 
 // create the server
 var wsServer = new WebSocketServer({
